ebfe7f5c13db17ad5bb8cc9946f859290160929b
raphell <raphael.luciano@outlook.fr>
2019-10-20 20:44:20 +0200

    delete useless print

--- src/main/scala/SgitApp.scala
+++ src/main/scala/SgitApp.scala
@@ -2,7 +2,6 @@ import CommandLineInterpreter.CommandParser._
 
 
 object SgitApp extends App{
-  println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA : "+ args)
   dispatch(args)
 }
 
